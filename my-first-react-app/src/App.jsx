ad4f0db12f27e741ca4ab4387a14cb29720a5848
tanmayma <tanmaym066@gmail.com>
2026-09-20 12:32:38 +0530

    Second Commit

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <>
       <h1>Welcome to React</h1>
+      <h2>Thank you</h2>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus, autem. Exercitationem nisi labore suscipit libero quo, accusamus sequi aliquid vitae perspiciatis laborum, explicabo voluptatibus atque. Obcaecati quidem voluptate esse exercitationem.</p>
       <h3>Thank You</h3>
       <Home />
